3ce0be3988a54c76b02d9cc1069969987ab3b5e1
Aleff Morais <aleff.morais@outlook.com>
2023-11-24 17:37:02 -0300

    Fixed syntax azapi again

--- services/aks-cluster/main.tf
+++ services/aks-cluster/main.tf
@@ -1,6 +1,9 @@
 terraform {
    required_version = ">= 1.1.0"
   required_providers {
+     azapi = {
+        source = "Azure/azapi"
+          }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.80.0"
@@ -8,10 +11,7 @@ terraform {
     random = {
         source = "hashicorp/random"
         version = "~> 3.5.1"
-        }
-    azapi = {
-        source = "Azure/azapi"
-          }
+        }   
     }
 }
 
